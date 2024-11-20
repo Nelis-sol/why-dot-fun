5171ceb2c902443bf07cf299e9cb3ea1ae9a92db
DKolter <danielkolter157@gmail.com>
2024-11-20 09:14:59 +0100

    Add IP based rate limit on /claim endpoint

--- src/claim/mod.rs
+++ src/claim/mod.rs
@@ -3,6 +3,8 @@ use axum::{
     routing::get,
     Router,
 };
+use std::sync::Arc;
+use tower_governor::{governor::GovernorConfig, GovernorLayer};
 
 mod page;
 mod verify;
@@ -11,4 +13,7 @@ pub fn router() -> Router {
     Router::new()
         .route("/", get(page::page_handler))
         .layer(middleware::from_fn(verify::verify))
+        .layer(GovernorLayer {
+            config: Arc::new(GovernorConfig::default()),
+        })
 }
