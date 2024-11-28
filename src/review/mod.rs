73082c54e02d4d175a2542984310bcddfcd51eba
DKolter <danielkolter157@gmail.com>
2024-11-28 15:39:56 +0100

    Convert draft review page to list view

--- src/review/mod.rs
+++ src/review/mod.rs
@@ -5,9 +5,9 @@ use axum::{
     extract::Request,
     http::StatusCode,
     middleware::{self, Next},
-    response::Response,
+    response::{Redirect, Response},
     routing::{get, post},
-    Extension, Json, Router,
+    Extension, Form, Router,
 };
 use axum_extra::extract::CookieJar;
 use reqwest::Client as ReqwestClient;
@@ -19,81 +19,78 @@ mod twitter;
 
 pub fn router() -> Router {
     Router::new()
-        .route("/", get(render_draft))
+        .route("/", get(review_page))
         .route("/approve", post(approve_draft))
         .route("/reject", post(reject_draft))
         .nest_service("/drafts", ServeDir::new("cache/drafts"))
         .layer(middleware::from_fn(check_token))
 }
 
-async fn render_draft() -> Result<DraftTemplate, StatusCode> {
+async fn review_page() -> Result<DraftTemplate, StatusCode> {
     Ok(DraftTemplate {
-        draft: get_next_draft().await.map_err(|e| {
-            log::error!("Failed to get next draft: {:?}", e);
+        drafts: get_drafts().await.map_err(|e| {
+            log::error!("Failed to get drafts: {:?}", e);
             StatusCode::INTERNAL_SERVER_ERROR
         })?,
     })
 }
 
-async fn get_next_draft() -> Result<Option<Draft>> {
+async fn get_drafts() -> Result<Vec<Draft>> {
     let mut dir = tokio::fs::read_dir("cache/drafts")
         .await
-        .context("Failed to read drafts directory")?;
+        .context("Reading drafts directory")?;
 
-    let entry = dir
-        .next_entry()
-        .await
-        .context("Failed to read next draft directory entry")?;
+    let mut drafts = Vec::new();
+    while let Some(entry) = dir.next_entry().await.context("Reading next draft entry")? {
+        let call_sid = entry
+            .file_name()
+            .into_string()
+            .map_err(|_| anyhow!("Failed to convert file name to string"))?
+            .trim_end_matches(".mp4")
+            .to_string();
 
-    let entry = match entry {
-        Some(entry) => entry,
-        None => return Ok(None),
-    };
+        let comment = tokio::fs::read_to_string(format!("cache/recordings/{call_sid}/comment.txt"))
+            .await
+            .context("Reading comment file")?;
 
-    let call_sid = entry
-        .file_name()
-        .into_string()
-        .map_err(|_| anyhow!("Failed to convert file name to string"))?
-        .trim_end_matches(".mp4")
-        .to_string();
+        drafts.push(Draft { call_sid, comment });
+    }
 
-    let comment = tokio::fs::read_to_string(format!("cache/recordings/{call_sid}/comment.txt"))
-        .await
-        .context("Failed to read comment file")?;
-
-    Ok(Some(Draft { call_sid, comment }))
+    Ok(drafts)
 }
 
 async fn approve_draft(
     twitter: Extension<TwitterApi<Oauth1aToken>>,
     reqwest: Extension<ReqwestClient>,
     secrets: Extension<Secrets>,
-    draft: Json<Draft>,
-) {
+    draft: Form<Draft>,
+) -> Redirect {
     log::debug!("Approving draft {}", draft.call_sid);
     let media_id = match twitter::upload_video(&reqwest, &secrets, &draft).await {
         Ok(media_id) => media_id,
         Err(e) => {
             log::error!("Failed to upload video: {:?}", e);
-            return;
+            return Redirect::to("/review");
         }
     };
 
     log::debug!("Posting tweet with media_id {}", media_id);
     if let Err(e) = twitter::post_tweet(&twitter, media_id, &draft).await {
         log::error!("Failed to post tweet: {:?}", e);
-        return;
+        return Redirect::to("/review");
     }
 
     log::debug!("Tweet posted successfully");
     let _ = tokio::fs::remove_file(format!("cache/drafts/{}.mp4", draft.call_sid)).await;
     let _ = tokio::fs::remove_dir_all(format!("cache/recordings/{}", draft.call_sid)).await;
+    Redirect::to("/review")
 }
 
-async fn reject_draft(draft: Json<Draft>) {
+async fn reject_draft(draft: Form<Draft>) -> Redirect {
     log::debug!("Rejecting draft {}", draft.call_sid);
     let _ = tokio::fs::remove_file(format!("cache/drafts/{}.mp4", draft.call_sid)).await;
-        let _ = tokio::fs::remove_dir_all(format!("cache/recordings/{}", draft.call_sid)).await;
+    let _ = tokio::fs::remove_dir_all(format!("cache/recordings/{}", draft.call_sid)).await;
+    Redirect::to("/review")
 }
 
 async fn check_token(
@@ -113,7 +110,7 @@ async fn check_token(
 #[derive(Debug, Deserialize, Serialize, Template)]
 #[template(path = "review.html")]
 pub struct DraftTemplate {
-    draft: Option<Draft>,
+    drafts: Vec<Draft>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
